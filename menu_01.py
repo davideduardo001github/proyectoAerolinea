b153b17371664c7c4b605897e299ea4d8484712b
davideduardo001github <davideduardo001@gmail.com>
2019-06-26 18:44:44 -0500

    Sistema de ingreso
    El sistema de ingreso al adminsitrador esta relalizado sin el uso de ***

--- menu_01.py
+++ menu_01.py
@@ -1,9 +1,33 @@
 """
 Menú principal
 """
-##  Generaciónd de un diccionario para el Administrador ##
+
+##  DECLARACIÓN DE MÉTODOS PARA EL PROGRAMA ##
 
 
+# Método "login" #
+"""
+ "login": retorna "True" si los datos fueron accesados correctamente, de lo contrario retorna "False"
+"""    
+def login(user,pasword):  
+    output1 = user in administradores
+    if output1 == True:
+        output2 = administradores[user] == pasword
+        if output2 == True:
+            print("Welcome: ",user)
+            return True
+        else:
+            print("Contraseña incorrecta")
+            return False
+    else:
+        print("user incorrecto")
+        return False
+
+
+## DEGLARACIÓN DE VARIABLES GLOBALES PARA EL PROGRAMA   ##
+
+# diccionario con admin y contraseña #
+administradores = {"Sharon":"sharon01","David":"david01"}
 
 
 ##  PROGRAMA PRINCIPAL  ##
@@ -12,14 +36,20 @@ while True:
     print("\t 1) Administrador")
     print("\t 2) Cliente")
     print("\t 3) Salir")
-    opcion = int(input("Escribe la opción deseada"))
+    opcion = int(input("Escribe la opción deseada: "))
     if opcion == 1:
-        pass
+        print("Para ingresar escribe tu usuario y contraseña de ADMINISTRADOR")
+        usuario = input("Usuario: ")
+        contraseña = input("Contraseña: ")
+        if login(usuario,contraseña) == True:
+            print("")
     elif opcion == 2:
         pass
     elif opcion == 3:
         print("Gracias por usar nuestra Aerolinea, vuelva pronto!")
         break
+    
+    print("\n")
 
 
 
